c5f87dfe5e86e012d6f26c8f486c41c6f274501c
Stacie Taylor-Cima <stacietaylorcima@Stacies-MacBook-Air.local>
2018-05-03 09:54:42 -0700

    update readme and optimizing documentation

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# kele-api-client
+# Kele
+Kele is a Ruby Gem API client to access the Bloc API.
+
+[Bloc's API Documentation](https://blocapi.docs.apiary.io/#)
+
+## Installation
+Add this line to your application's Gemfile:
+```
+gem 'kele'
+```
+
+And then execute:
+```
+$ bundle
+```
+Or install it yourself as:
+```
+$ gem install kele
+```
+
+## Usage
+The client can be used as follows, (Populated with your Bloc.io email and password to access the API.):
+```
+$ irb
+>> require './lib/kele'
+=> true
+>> Kele.new("jane@gmail.com", "abc123")
+```
+To retrieve a parsed ruby hash of your Bloc.io account information then run:
+```
+$ kele_client.get_me
+```
+
+To retrieve your mentor's current availability:
+```
+$ kele_client.get_mentor_availability(mentor_id)
+```
+
+To retrieve your Bloc.io roadmap:
+```
+$ kele_client.get_roadmap(chain_id)
+```
+
+To retrieve information on a particular checkpoint:
+```
+$ kele_client.get_checkpoint(checkpoint_id)
+```
+
+To retreive your remaining checkpoints:
+```
+$ kele_client.get_remaining_checkpoints(chain_id)
+```
+
+To retrieve your messages:
+```
+$ kele_client.get_messages(1) # returns the first page of message threads
+$ kele_client.get_messages # returns the first page of messages
+```
+
+To send a message:
+```
+$ kele.client.create_message(sender, recipient_id, token, subject, text)
+```
--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,6 +1,6 @@
 module Roadmap
   # Retrieves roadmap data for current users
-  # Params: chain_id = integer | chain_id can be retrived by running the get_me method on the current user. It is found listed under current enrollement/chain_id
+  # Params: chain_id = integer | chain_id can be retrived by running the get_me method on the current user. It is found listed under current_enrollement/chain_id
   def get_roadmap(chain_id)
     # Point the HTTParty GET method at the roadmaps/#{chain_id} endpoint of Bloc's API.
     # Use HTTParty's header option to pass the auth_token.
@@ -9,6 +9,8 @@ module Roadmap
     JSON.parse(response.body)
   end
 
+  # Retrieves individual checkpoint data
+  # Params: checkpoint_id = integer | checkpoint_id can be retrived by running the get_roadmap method on the current user. It is found listed under sections/checkpoint/id.
   def get_checkpoint(checkpoint_id)
     # Point the HTTParty GET method at the checkpoints/#{checkpoint_id} endpoint of Bloc's API.
     # Use HTTParty's header option to pass the auth_token.
@@ -17,8 +19,13 @@ module Roadmap
     JSON.parse(response.body)
   end
 
+  # Retrieves the remaining checkpoints for the current user
+  # Params: chain_id = integer | chain_id can be retrived by running the get_me method on the current user. It is found listed under current_enrollement/chain_id
   def get_remaining_checkpoints(chain_id)
+    # Point the HTTParty GET method at the enrollment_chains/#{chain_id}/checkpoints_remaining_in_section endpoint of Bloc's API.
+    # Use HTTParty's header option to pass the auth_token.
     response = self.class.get(api_url("enrollment_chains/#{chain_id}/checkpoints_remaining_in_section"), headers: { "authorization" => @auth_token })
+    # Parse the JSON document returned in the response into a Ruby hash.
     JSON.parse(response.body)
   end
 end
